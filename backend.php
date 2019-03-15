a87b203b6bded06ef1f015250ace937f93f24001
Jasmina Travnjak <animsai@gmail.com>
2019-03-15 18:49:57 +0100

    added functions for getting all references of a title and references by category

--- backend.php
+++ backend.php
@@ -4,6 +4,7 @@
  */
 header('Content-type: application/json');
 
+
 /**
  * @name : the name of the title
  * return :  Main informaitons of the title
@@ -21,4 +22,52 @@ function getTitleByName($name){
   return($array);
 }
 
-getTitleByName("Nés sous la même étoile");
+/***
+ * @param $idTitle id of the title we want to have the refs
+ * @return string json encoded table containing the sum of the references found for this title
+ */
+function getAllRefsByTitleId($idTitle){
+
+    $refArray = [
+        "Musique" => "11",
+        "Cinéma" => "6",
+        "Politique" => "2",
+        "Autre" => ""
+    ];
+
+    return json_encode($refArray);
+    }
+
+ function getReferencesForTitle($idTitle, $idCategory){
+    $refArrays = array();
+
+    $refArray[0] = [
+        "id" => "1",
+        "title" => "Goldorak",
+        "category" => "Cinema",
+        "categoryId" => "3",
+        "desc" => "A la fin du morceau, on peut entendre un extrait du deuxième épisode de Goldorak : une phrase prononcée par Actarus, « Mon Dieu, pourquoi ne puis-je vivre comme n'importe quel être humain ? Pourquoi mon destin est-il de ne pouvoir cesser de me battre ? ».",
+        "url" => ""
+    ] ;
+
+    $refArray[1] = [
+        "id" => "2",
+        "title" => "Scarface",
+        "category" => "Cinema",
+        "categoryId" => "3",
+        "desc" => "On entend à la toute fin du morceau la phrase « La vie de rêve ». C'est une citation de Tony Montana joué par Al Pacino dans la version française du film Scarface de Brian De Palma. Le personnage prononce cette phrase lors de sa demande d'asile politique aux États-Unis alors qu'il dépeint son quotidien à subir le pouvoir communiste de Cuba qu'il vient de quitter.",
+        "url" => ""
+        ];
+
+        $refArray[2] = [
+            "id" => "3",
+            "title" => "Meurtre à Alcatraz",
+            "category" => "Cinema",
+            "categoryId" => "3",
+            "desc" => "dslafjsa",
+            "url" => ""
+        ];
+
+return json_encode($refArray);
+
+}
