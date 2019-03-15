ef5e88507a59ce190c05a01435091856e032ce79
ExtraDev <scott.birner@gmail.com>
2019-03-15 18:42:37 +0100

    create getTitleByName method. Return main informations of the title

--- backend.php
+++ backend.php
@@ -3,3 +3,22 @@
  * Date : 15.03.2019
  */
 header('Content-type: application/json');
+
+/**
+ * @name : the name of the title
+ * return :  Main informaitons of the title
+ * return-type: json
+ */
+function getTitleByName($name){
+  $array = [
+    "id"=>"14",
+    "name"=>"Nés sous la même étoile",
+    "artise"=> "Iam",
+    "link"=>"https://open.spotify.com/track/66ZtqKhYSA8XyPr0aAUFsm",
+    "release_date"=> "2017-05-26"
+  ];
+
+  return($array);
+}
+
+getTitleByName();
