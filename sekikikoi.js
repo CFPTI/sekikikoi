a0490063ecbed6f8bfb21ddccaba6514b0fd394a
Jasmina Travnjak <animsai@gmail.com>
2019-03-22 15:19:14 +0100

    Fixed chart and added categories get relations

--- sekikikoi.js
+++ sekikikoi.js
@@ -3,105 +3,46 @@ window.onload = function() {
     google.charts.setOnLoadCallback(init);
 };
 
-function init(){
-    var musicPlayer = Vue.component('music-player', {
-        props: ['music-link'],
-        template:  '<iframe :src="musicLink" width="100%" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>'
-    });
+function init() {
+    var dataArray = [
+        ['Musique', 11],
+        ['Cinéma', 2],
+        ['Politique', 2],
+        ['Autre', 2]
+    ];
+    var myChart = drawChart(dataArray);
 
-    var $page = $('#wrapper'),
-        options = {
-            debug: true,
-            prefetch: true,
-            cacheLength: 2,
-            onStart: {
-                duration: 250, // Duration of our animation
-                render: function ($container) {
-                    // Add your CSS animation reversing class
-                    $container.addClass('is-exiting');
-                    // Restart your animation
-                    smoothState.restartCSSAnimations();
-                }
-            },
-            onReady: {
-                duration: 0,
-                render: function ($container, $newContent) {
-                    // Remove your CSS animation reversing class
-                    $container.removeClass('is-exiting');
-                    // Inject the new content
-                    $container.html($newContent);
-                }
-            }
-        },
-        smoothState = $page.smoothState(options).data('smoothState');
+    google.visualization.events.addListener(myChart, 'select', function() {
+        console.log(myChart.getSelection());
 
-    var musicChart = Vue.component('music-chart', {
-        data() {
-            return {
-                isChartHidden:false,
-                refChartData : google.visualization.arrayToDataTable([
-                    ['Catégorie', 'Nb de références'],
-                    ['Musique',     11],
-                    ['Cinéma',      2],
-                    ['Politique',  2],
-                    ['Autre', 2]
-                ]),
-                refChartOptions : {
-                    legend:"none",
-                    title: '',
-                    pieSliceText:'label',
-                    animation: {
-                        duration: 1000,
-                        easing: 'in',
-                        startup: true
-                    }
-                },
-                refChart : null,
-                refChartId:"refPieChart"
-            }
-        },
-        template:  '<div :id="refChartId" style="width: 900px; height: 500px;"></div>',
-        mounted() {
-           
-            google.visualization.events.addListener(this.refChart, 'select', function() {
-                console.log("select cat " + that.refChart.getSelection());
-                showPage('detailRef');
-            });
-            this.initChart();
-        },
-        methods:{
-            showPage : function(page){
-
-            },
-            initChart(){
-                var that = this;
-                $.ajax ({
-                    url:"./backend.php",
-                    data: {"methodName":"getAllRefsByTitleId"},
-                    success: (data) => {
-                        console.log(data);
-                        data = new google.visualization.DataTable(data);
-                        that.refChartData = data;
-                        that.refChart = new google.visualization.PieChart(document.getElementById(that.refChartId));
-                        that.refChart.draw(that.refChartData, that.refChartOptions);
-                    }
-                });
-            }
-        }
-    });
-
-    var app = new Vue({
-        el:'#sekikikoiApp',
-        methods: {
-            showChart:function() {
-                musicChart.props.isChartHidden = false;
-            }
-        }
+        window.location = "./detailPolitique.html?cat=" + dataArray[myChart.getSelection()[0].row][0];
     });
 }
 
-/*
-click
-google.visualization.events.addListener(chart, 'select', function() {
-    console.log("select cat " + chart.getSelection());
-});*/
+function drawChart(dataArray) {
+    var isChartHidden = false;
+
+    var refChartOptions = {
+        legend: "none",
+        title: '',
+        pieSliceText: 'label',
+        animation: {
+            duration: 1000,
+            easing: 'in',
+            startup: true
+        }
+    };
+    var refChart = null;
+    var refChartId = "refChart";
+
+    var refChartData = new google.visualization.DataTable();
+    refChartData.addColumn('string', 'Catégorie');
+    refChartData.addColumn('number', 'Nb de références');
+
+    refChartData.addRows(dataArray);
+
+    refChart = new google.visualization.PieChart(document.getElementById(refChartId));
+    refChart.draw(refChartData, refChartOptions);
+    return refChart;
+}
+
