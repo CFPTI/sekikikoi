e50c4bac971a45d1545c12c561ffc9a9f2ea1f5c
ExtraDev <scott.birner@gmail.com>
2019-03-22 18:27:55 +0100

    getReferencesForTitle retourne toutes les ref de la bd

--- backend/backend.php
+++ backend/backend.php
@@ -28,7 +28,7 @@ if(isset($_GET["methodName"])){
 
     if($methodeName == "getReferencesForTitle"){
       
-      echo getReferencesForTitle(1);
+      echo getReferencesForTitle(1,2);
     }
 }
 
@@ -53,15 +53,15 @@ function getTitleByName($name){
  * @param $idTitle id of the title we want to have the refs
  * @return string json encoded table containing the sum of the references found for this title
  */
-function getAllRefsByTitleId($idTitle){
+function getAllRefsByTitleId($idMedia){
 
     $pdoStmt = EDatabase::prepare("
-    SELECT COUNT(id_catergory), category.name
+    SELECT COUNT(category.id_category), category.name
     FROM `reference`, `media`, `category`
     WHERE id_media_ref = id_media
-    AND media.id_catergory = category.id_category
+    AND media.id_category = category.id_category
     AND `id_media_base` = :idMedia
-    GROUP BY (id_catergory)
+    GROUP BY (category.id_category)
    ");
    $pdoStmt->bindParam(':idMedia', $idMedia, PDO::PARAM_INT);
    $pdoStmt->execute();
@@ -69,43 +69,18 @@ function getAllRefsByTitleId($idTitle){
    return json_encode($refArray);
   }
 
- function getReferencesForTitle($idMedia){
- /* $pdoStmt = EDatabase::prepare("");
+ function getReferencesForTitle($idMedia, $idCategory){
+  $pdoStmt = EDatabase::prepare("Select * from reference, media, category where 
+  id_media_ref = id_media
+  AND media.id_category = category.id_category 
+  AND id_media_base = :idMedia
+  AND category.id_category = :idCategory");
+
   $pdoStmt->bindParam(':idMedia', $idMedia, PDO::PARAM_INT);
+  $pdoStmt->bindParam(':idCategory', $idCategory, PDO::PARAM_INT);
   $pdoStmt->execute();
- */
-    /*$refArrays = array();
-
-    $refArrays[0] = [
-        "id" => "1",
-        "title" => "Goldorak",
-        "category" => "Cinema",
-        "categoryId" => "3",
-        "typeRef" => "Citation",
-        "desc" => "A la fin du morceau, on peut entendre un extrait du deuxième épisode de Goldorak : une phrase prononcée par Actarus, « Mon Dieu, pourquoi ne puis-je vivre comme n'importe quel être humain ? Pourquoi mon destin est-il de ne pouvoir cesser de me battre ? ».",
-        "url" => "#"
-    ] ;
-
-    $refArrays[1] = [
-        "id" => "2",
-        "title" => "Scarface",
-        "category" => "Cinema",
-        "categoryId" => "3",
-        "desc" => "On entend à la toute fin du morceau la phrase « La vie de rêve ». C'est une citation de Tony Montana joué par Al Pacino dans la version française du film Scarface de Brian De Palma. Le personnage prononce cette phrase lors de sa demande d'asile politique aux États-Unis alors qu'il dépeint son quotidien à subir le pouvoir communiste de Cuba qu'il vient de quitter.",
-        "typeRef" => "Citation",
-        "url" => "#"
-        ];
-
-    $refArrays[2] = [
-        "id" => "3",
-        "title" => "Meurtre à Alcatraz",
-        "category" => "Cinema",
-        "categoryId" => "3",
-        "typeRef" => "Sample",
-        "desc" => "Le sample de ce morceau est tiré de ce film. Il aparaît à la fin du film notamment",
-        "url" => "#"
-    ];
-
-return json_encode($refArray);
-*/
+  $refArrays = array();
+  $refArrays = $pdoStmt->fetchAll(PDO::FETCH_ASSOC);
+  
+  return json_encode($refArrays);
 }
