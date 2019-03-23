ef5a71e74a9bc80f8df047aed7846717e9f45a3d
TonyIsHere <anthony.chvrl@eduge.ch>
2019-03-23 11:35:35 +0100

    fix font size chart

--- sekikikoi.js
+++ sekikikoi.js
@@ -164,7 +164,7 @@ $(document).ready(function () {
             legend: "none",
             title: '',
             pieSliceText: 'label',
-            fontSize: 26,
+            fontSize: 23,
             colors: ['#CC0066', '#CC3399', '#CC66CC', '#99FF00'],
             animation: {
                 duration: 1000,
